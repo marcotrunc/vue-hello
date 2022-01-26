6011955120ef25dad107bfab51e73eaeb78be84d
marcotrunc <marcotrunc@gmail.com>
2022-01-26 14:07:49 +0100

    added button that changes the theme

--- js/script.js
+++ js/script.js
@@ -10,7 +10,24 @@ Aggiungere alla pagina un’immagine, presa anch’essa da un data. */
 const app = new Vue({
     el: '#root',
     data: {
-        message: 'Hello Vue!',
+        message: "Hello Vue!",
         image: 'https://picsum.photos/200/300',
+        alt: "generic img",
+        color: "text-white",
+        bgColor: "bg-dark",
+        btnColor: "btn-light"
+    },
+    methods: {
+        changeButtonLight() {
+            if (this.bgColor === "bg-dark") {
+                this.bgColor = "bg-light";
+                this.color = "text-dark";
+                this.btnColor = "btn-dark";
+            } else {
+                this.bgColor = "bg-dark";
+                this.color = "text-white";
+                this.btnColor = "btn-light";
+            }
+        }
     }
 });
